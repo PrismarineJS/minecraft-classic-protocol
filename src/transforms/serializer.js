6d4d02830010860379c1426d54a5c13305d933a0
mhsjlw <mhsjlw@aol.com>
2015-11-21 09:16:54 -0500

    Created serializer

---
+++ src/transforms/serializer.js
@@ -0,0 +1,69 @@
+var ProtoDef = require("protodef").ProtoDef;
+var Serializer = require("protodef").Serializer;
+var Parser = require("protodef").Parser;
+
+var minecraft = require("../datatypes/minecraft");
+
+function createProtocol(types, packets) {
+  var proto = new ProtoDef();
+
+  proto.addType("string", ["pstring", {
+    countType: "varint"
+  }]);
+
+  proto.addTypes(minecraft);
+  proto.addTypes(types);
+
+  Object.keys(packets).forEach(function (name) {
+    proto.addType("packet_" + name, ["container", packets[name].fields]);
+  });
+
+  proto.addType("packet", ["container", [{
+    "name": "name",
+    "type": ["mapper", {
+      "type": "varint",
+      "mappings": Object.keys(packets).reduce(function (acc, name) {
+        acc[parseInt(packets[name].id)] = name;
+        return acc;
+      }, {})
+    }]
+  }, {
+    "name": "params",
+    "type": ["switch", {
+      "compareTo": "name",
+      "fields": Object.keys(packets).reduce(function (acc, name) {
+        acc[name] = "packet_" + name;
+        return acc;
+      }, {})
+    }]
+  }]]);
+  return proto;
+}
+
+function createSerializer({
+  isServer = false, version
+} = {}) {
+  var mcData = require("../../data/protocol");
+  var direction = !isServer ? 'toServer' : 'toClient';
+  var packets = mcData[direction];
+  var proto = createProtocol(packets);
+  return new Serializer(proto, "packet");
+}
+
+function createDeserializer({
+  isServer = false,
+    packetsToParse = {
+      "packet": true
+    }, version
+} = {}) {
+  var mcData = require("../../data/protocol");
+  var direction = isServer ? "toServer" : "toClient";
+  var packets = mcData[direction];
+  var proto = createProtocol(packets);
+  return new Parser(proto, "packet");
+}
+
+module.exports = {
+  createSerializer: createSerializer,
+  createDeserializer: createDeserializer
+};
